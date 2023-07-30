3eda8e40277efa92e7e4a8e0e1700208e6da8745
kirill.klimonov <kklimonov@yandex.ru>
2023-07-30 22:17:01 +0300

    Added new functonal: added db migration

--- AuthService/src/main/java/com/sdi/config/AuthorizationServerConfig.java
+++ AuthService/src/main/java/com/sdi/config/AuthorizationServerConfig.java
@@ -1,23 +1,38 @@
 package com.sdi.config;
 
+import com.sdi.dto.AuthorizedUser;
+import com.sdi.dto.IntrospectionPrincipal;
+import com.sdi.dto.TokenInfoDto;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.OAuth2Authorization;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenIntrospection;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
+import org.springframework.security.oauth2.server.authorization.authentication.OAuth2TokenIntrospectionAuthenticationToken;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
+import java.io.IOException;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
@@ -25,15 +40,26 @@ import java.time.temporal.ChronoUnit;
 @Configuration(proxyBeanMethods = false)
 public class AuthorizationServerConfig {
 
+    private final static String principalAttributeKey = "java.security.Principal";
+
+    private final MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter;
     private final AuthorizationServerProperties authorizationServerProperties;
+    private final OAuth2AuthorizationService oAuth2AuthorizationService;
 
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
     public SecurityFilterChain authServerSecurityFilterChain(HttpSecurity http) throws Exception {
-        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
-        http.exceptionHandling(exceptions ->
-                exceptions.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
-        );
+        OAuth2AuthorizationServerConfigurer authorizationServerConfigurer = new OAuth2AuthorizationServerConfigurer();
+        authorizationServerConfigurer.tokenIntrospectionEndpoint((config) -> {
+            config.introspectionResponseHandler(this::introspectionResponse);
+        });
+
+        RequestMatcher endpointsMatcher = authorizationServerConfigurer.getEndpointsMatcher();
+        http.securityMatcher(endpointsMatcher)
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .csrf(csrf -> csrf.ignoringRequestMatchers(endpointsMatcher))
+                .exceptionHandling(exceptions -> exceptions.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login")))
+                .apply(authorizationServerConfigurer);
         return http.build();
     }
 
@@ -62,19 +88,47 @@ public class AuthorizationServerConfig {
         );
     }
 
-
-//    @Bean
-//    public JWKSource<SecurityContext> jwkSource() {
-//        RSAKey rsaKey = JwkUtils.generateRsa();
-//        JWKSet jwkSet = new JWKSet(rsaKey);
-//        return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
-//    }
-
     @Bean
     public AuthorizationServerSettings authorizationServerSettings() {
         return AuthorizationServerSettings.builder()
                 .issuer(authorizationServerProperties.getIssuerUrl())
                 .tokenIntrospectionEndpoint(authorizationServerProperties.getIntrospectionEndpoint())
                 .build();
+    }
+
+    private void introspectionResponse(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        OAuth2TokenIntrospectionAuthenticationToken introspectionAuthenticationToken = (OAuth2TokenIntrospectionAuthenticationToken) authentication;
+        TokenInfoDto.TokenInfoDtoBuilder tokenInfoDtoBuilder = TokenInfoDto.builder().active(false);
+        if (introspectionAuthenticationToken.getTokenClaims().isActive()) {
+            OAuth2TokenIntrospection claims = introspectionAuthenticationToken.getTokenClaims();
+            tokenInfoDtoBuilder.active(true)
+                    .sub(claims.getSubject())
+                    .aud(claims.getAudience())
+                    .nbf(claims.getNotBefore())
+                    .scopes(claims.getScopes())
+                    .iss(claims.getIssuer())
+                    .exp(claims.getExpiresAt())
+                    .iat(claims.getIssuedAt())
+                    .jti(claims.getId())
+                    .clientId(claims.getClientId())
+                    .tokenType(claims.getTokenType());
+
+
+            String token = introspectionAuthenticationToken.getToken();
+            OAuth2Authorization tokenAuth = oAuth2AuthorizationService.findByToken(token, OAuth2TokenType.ACCESS_TOKEN);
+            if (tokenAuth != null) {
+                Authentication attributeAuth = tokenAuth.getAttribute(principalAttributeKey);
+                if (attributeAuth != null) {
+                    if (attributeAuth.getPrincipal() instanceof AuthorizedUser authorizedUser) {
+                        tokenInfoDtoBuilder.principal(IntrospectionPrincipal.build(authorizedUser));
+                    } else {
+                        throw new RuntimeException("Principal class = " + attributeAuth.getPrincipal().getClass().getSimpleName() + " is not supported");
+                    }
+                }
+            }
+        }
+
+        ServletServerHttpResponse httpResponse = new ServletServerHttpResponse(response);
+        mappingJackson2HttpMessageConverter.write(tokenInfoDtoBuilder.build(), null, httpResponse);
     }
 }
